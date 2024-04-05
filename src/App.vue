478b6d4aa99163cbda45696df6345b369fffabbd
pahu2353 <pahu2353@gmail.com>
2024-04-04 23:30:43 -0400

    Initial chessboard created with basic responsiveness

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <img src="./assets/darkLogo.png" id="logo">
-  <ChessBoard/>
-  <SideBar />
+  <div id="app">
+    <div id="logo">
+      <img src="./assets/darkLogo.png">
+    </div>
+  <div id="container">
+    <ChessBoard/>
+    <SideBar />
+  </div>
+</div>
 </template>
 
 <script>
@@ -19,6 +25,7 @@ export default {
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,20 +33,37 @@ export default {
   color: #2c3e50;
   
   /* theme colours: inspired by chess.com (inspect element -> stylesheet) */
+  --background-colour: #302E2B;
   --dark-square: #739552;
   --light-square: #EBECD0;
   --highlight: #ffff33;
+  --sidebar-header: #666564;
+  --sidebar-background: #ffffff;
+}
 
+#container{
   /* to handle requirements 2 and 3, responsive positioning */
   display: flex;
   flex-direction: row;
 }
+
 #logo{
   /* preliminary css styling for the logo */
-  margin-top: 10px;
-  margin-left: 10px;
-  width: 10%;
-  height: 10%;
+  padding: 10px;
+}
+
+#logo img {
+
+  /* temporary values, to be changed later */
+  width: 183.4px;
+  height: 50.8px;
+}
+
+/* breakpoint for switching sidebar to below, inspired by https://www.chess.com/analysis */
+@media (max-width: 960px) {
+  #app {
+    flex-direction: column;
+  }
 }
 
 </style>
--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="chessboard">here is the chessboard</div>
-</template>
+   <div class="chessboard">
+        <div v-for="rank in 8" :key="'rank' + rank" class="rank">
+            <BoardSquares 
+            v-for="file in 8" 
+            :key="'file' + file" class="file"
+            :isLight="(rank + file) % 2 == 0"/>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  import BoardSquares from './BoardSquares.vue';
+  
+  export default {
+    components: {
+      BoardSquares
+    }
+  }
+  </script>
+  
+  <style>
+  .chessboard {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .rank {
+    display: flex;
+  }
 
 
+  </style>
